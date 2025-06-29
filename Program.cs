1fbcde89f943ec82068e2b7e6316c3bbd2148f99
YolgeSanchez <imjorgesanchez@gmail.com>
2025-06-29 00:40:57 -0400

    chore: install and use required dependencies

--- Program.cs
+++ Program.cs
@@ -1,9 +1,41 @@
 ﻿using System;
 using Helpers;
+using Spectre.Console;
+using Humanizer;
+using Newtonsoft.Json;
 
 internal class Program {
   private static void Main(string[] args) {
-    // saludo
+    /*
+    var variable = AnsiConsole.Prompt(new ConfirmationPrompt(string));
+
+    var variable = AnsiConsole.Prompt(new TextPrompt<T>(string));
+    
+    var variable = AnsiConsole.Prompt(
+      new SelectionPrompt<T>()
+      .Title(string)
+      .PageSize(int)
+      .MoreChoicesText(string)
+      .AddChoices([])
+    );
+
+    var table = new Table();
+
+    table.AddColumn(new TableColumn(string).Centered());
+    table.AddRow(string[]);
+
+    table.ShowRowSeparators();
+
+    table.Alignment(Justify.Right);
+    table.RightAligned();
+    table.Centered();
+    table.LeftAligned();
+
+    table.Border(TableBorder.Rounded);
+    */
+
+    
+
   }
 }
 
