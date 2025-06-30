8d1276dc415fcb59daace5fe5d35314aaad35512
YolgeSanchez <imjorgesanchez@gmail.com>
2025-06-30 01:55:59 -0400

    refactor: clean some variables naming and comments

--- helpers.cs
+++ helpers.cs
@@ -2,12 +2,9 @@ using Spectre.Console;
 
 namespace Helpers {
   public static class array {
-    // add an element to an array
     public static string[] add(string[] arr, string val) {
-      // validate duplicates
-      if (Array.IndexOf(arr, val) != -1) throw new Exception("Valor duplicado");
+      if (Array.IndexOf(arr, val) != -1) throw new Exception("[red]Valor duplicado[/]");
 
-      // add element
       int len = arr.Length;
       string[] new_arr = new string[len + 1];
       new_arr[len] = val;
@@ -19,22 +16,16 @@ namespace Helpers {
       return new_arr;
     }
 
-    // edit an element from an array
     public static void edit(string[] arr, string val, int idx) {
-      // validate duplicates
-      if (Array.IndexOf(arr, val) != -1) throw new Exception("Valor duplicado");
+      if (Array.IndexOf(arr, val) != -1) throw new Exception("[red]Valor duplicado[/]");
 
-      // edit element
       arr[idx] = val;
     }
 
-    // delete an element from an array
     public static string[] delete(string[] arr, int idx) {
-      // validate index
       int len = arr.Length;
-      if (idx >= len--) throw new Exception("Indice fuera de rango");
+      if (idx >= len--) throw new Exception("[red]Indice fuera de rango[/]");
 
-      // delete element
       string[] new_arr = new string[len];
 
       for (int oldidx = 0, newidx = 0; oldidx < len; oldidx++) {
@@ -54,19 +45,6 @@ namespace Helpers {
       return new_arr;
     }
   }
-
-  // other helpers maybe later
-  /*
-
-  table.ShowRowSeparators();
-
-  table.Alignment(Justify.Right);
-  table.RightAligned();
-  table.Centered();
-  table.LeftAligned();
-
-  table.Border(TableBorder.Rounded);
-  */
 
   public static class console {
     public static bool read_confirm(string msg) {
@@ -121,15 +99,17 @@ namespace Helpers {
       return table;
     }
 
-    public static void add_rows(Table table, string[,] rows) {
-      for(int rowidx = 0; rowidx < rows.GetLength(0); rowidx++) {
-        string[] row = new string[rows.GetLength(1)];
-        
-        for (int colidx = 0; colidx < rows.GetLength(1); colidx++) {
-          row[colidx] = rows[rowidx, colidx];
+    public static void add_rows(Table table, string[,] data) {
+      int rows = data.GetLength(0);
+      int cols = data.GetLength(1);
+      for(int row = 0; row < rows; row++) {
+        string[] data_row = new string[cols];
+
+        for (int col = 0; col < cols; col++) {
+          data_row[col] = data[row, col];
         }
 
-        table.AddRow(row);
+        table.AddRow(data_row);
       }
     }
 
