6c5fdc267dc8c003e6b3bed6cbeffb4b44bd54b2
YolgeSanchez <imjorgesanchez@gmail.com>
2025-07-05 02:58:48 -0400

    feat: initialize students array with some data to enhance testing

--- Data.cs
+++ Data.cs
@@ -1,6 +1,12 @@
 namespace Clases {
   public static class Data {
     public static string[] roles = {"Developer", "Designer", "Leader"};
-    public static Student[] students = new Student[0];
+    // public static Student[] students = new Student[0];
+    static Student jorge = new Student("Jorge");
+    static Student Joa = new Student("Ngga Jr");
+    static Student junito = new Student("Junito");
+    static Student oriam = new Student("Oriam");
+    static Student henry = new Student("Hynox");
+    public static Student[] students = {jorge, Joa, junito, oriam, henry};
   }
 }
